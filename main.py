88b5ceb2e9bf677f8fcd08971cdaca7f3c406502
GeorgeSeif <george.seif94@gmail.com>
2018-03-04 08:49:43 -0500

    Trained mobile net. Verified predictions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+# Networks
 from keras.preprocessing import image
 from keras.applications.resnet50 import ResNet50
 from keras.applications.vgg16 import VGG16
@@ -12,9 +13,11 @@ from keras.applications.densenet import DenseNet121, DenseNet169, DenseNet201
 from keras.applications.nasnet import NASNetLarge, NASNetMobile
 from keras.preprocessing.image import ImageDataGenerator
 
+# Layers
 from keras.layers import Dense, Activation, Flatten, Dropout
 from keras import backend as K
 
+# Other
 from keras import optimizers
 from keras import losses
 from keras.optimizers import SGD, Adam
@@ -22,15 +25,16 @@ from keras.models import Sequential, Model
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras.models import load_model
 
-
+# Utils
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import random, glob
 import os, sys, csv
 import cv2
+import time, datetime
 
-
+# For boolean input from the command line
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -40,26 +44,28 @@ def str2bool(v):
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
+# Command line args
 parser = argparse.ArgumentParser()
-parser.add_argument('--num_epochs', type=int, default=300, help='Number of epochs to train for')
+parser.add_argument('--num_epochs', type=int, default=20, help='Number of epochs to train for')
 parser.add_argument('--mode', type=str, default="train", help='Select "train", or "predict" mode. \
     Note that for prediction mode you have to specify an image to run the model on.')
 parser.add_argument('--image', type=str, default=None, help='The image you want to predict on. Only valid in "predict" mode.')
 parser.add_argument('--continue_training', type=str2bool, default=False, help='Whether to continue training from a checkpoint')
 parser.add_argument('--dataset', type=str, default="Pets", help='Dataset you are using.')
-parser.add_argument('--resize_height', type=int, default=256, help='Height of cropped input image to network')
-parser.add_argument('--resize_width', type=int, default=256, help='Width of cropped input image to network')
+parser.add_argument('--resize_height', type=int, default=224, help='Height of cropped input image to network')
+parser.add_argument('--resize_width', type=int, default=224, help='Width of cropped input image to network')
 parser.add_argument('--batch_size', type=int, default=32, help='Number of images in each batch')
-parser.add_argument('--dropout', type=float, default=0.5, help='Dropout ratio')
+parser.add_argument('--dropout', type=float, default=1e-3, help='Dropout ratio')
 parser.add_argument('--h_flip', type=str2bool, default=False, help='Whether to randomly flip the image horizontally for data augmentation')
 parser.add_argument('--v_flip', type=str2bool, default=False, help='Whether to randomly flip the image vertically for data augmentation')
 parser.add_argument('--rotation', type=float, default=0.0, help='Whether to randomly rotate the image for data augmentation')
 parser.add_argument('--zoom', type=float, default=0.0, help='Whether to randomly zoom in for data augmentation')
 parser.add_argument('--shear', type=float, default=0.0, help='Whether to randomly shear in for data augmentation')
-parser.add_argument('--model', type=str, default="VGG16")
+parser.add_argument('--model', type=str, default="MobileNet")
 args = parser.parse_args()
 
 
+# Global settings
 BATCH_SIZE = args.batch_size
 WIDTH = args.resize_width
 HEIGHT = args.resize_height
@@ -70,7 +76,9 @@ VAL_DIR = args.dataset + "/val/"
 preprocessing_function = None
 base_model = None
 
-def save_classes(class_list):
+
+def save_class_list(class_list):
+    class_list.sort()
     target=open("./checkpoints/" + args.model + "_" + args.dataset + "_class_list.txt",'w')
     for c in class_list:
         target.write(c)
@@ -78,21 +86,21 @@ def save_classes(class_list):
 
 def load_class_list(class_list_file):
     class_list = []
-    with open(class_list_file, 'rb') as csvfile:
+    with open(class_list_file, 'r') as csvfile:
         file_reader = csv.reader(csvfile)
         for row in file_reader:
             class_list.append(row)
+    class_list.sort()
     return class_list
 
-
-
-
-def get_subfolders(dataset):
-    subfolders = os.listdir(dataset)
+# Get a list of subfolders in the directory
+def get_subfolders(directory):
+    subfolders = os.listdir(directory)
+    subfolders.sort()
     return subfolders
 
+# Get number of files by searching directory recursively
 def get_num_files(directory):
-    """Get number of files by searching directory recursively"""
     if not os.path.exists(directory):
         return 0
     cnt = 0
@@ -101,7 +109,7 @@ def get_num_files(directory):
             cnt += len(glob.glob(os.path.join(r, dr + "/*")))
     return cnt
 
-
+# Add on new FC layers with dropout for fine tuning
 def build_finetune_model(base_model, num_classes):
     for layer in base_model.layers:
         layer.trainable = False
@@ -118,6 +126,7 @@ def build_finetune_model(base_model, num_classes):
 
     return finetune_model
 
+# Plot the training and validation loss + accuracy
 def plot_training(history):
     acc = history.history['acc']
     val_acc = history.history['val_acc']
@@ -228,14 +237,17 @@ if args.mode == "train":
 
     validation_generator = val_datagen.flow_from_directory(VAL_DIR, target_size=(HEIGHT, WIDTH), batch_size=BATCH_SIZE)
 
+
+    # Save the list of classes for prediction mode later
     class_list = get_subfolders(TRAIN_DIR)
+    save_class_list(class_list)
 
     finetune_model = build_finetune_model(base_model, len(class_list))
 
     if args.continue_training:
-        finetune_model.load_weights("./checkpoints/" + args.model + "_" + args.dataset + "_model_weights.h5")
+        finetune_model.load_weights("./checkpoints/" + args.model + "_model_weights.h5")
 
-    adam = Adam(lr=0.0001)
+    adam = Adam(lr=0.00001)
     finetune_model.compile(adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
     num_train_images = get_num_files(TRAIN_DIR)
@@ -254,40 +266,50 @@ if args.mode == "train":
     checkpoint = ModelCheckpoint(filepath, monitor=["acc"], verbose=1, mode='max')
     callbacks_list = [checkpoint]
 
+    print(class_list[0], " = ", len(os.listdir((args.dataset + "/train/" + class_list[0]))))
+    print(class_list[1], " = ", len(os.listdir((args.dataset + "/train/" + class_list[1]))))
 
-    history = finetune_model.fit_generator(train_generator, epochs=args.num_epochs, workers=8, steps_per_epoch=num_train_images, 
-        validation_data=validation_generator, validation_steps=num_val_images, class_weight='auto', shuffle=True, callbacks=callbacks_list)
+
+    history = finetune_model.fit_generator(train_generator, epochs=args.num_epochs, workers=8, steps_per_epoch=num_train_images // BATCH_SIZE, 
+        validation_data=validation_generator, validation_steps=num_val_images // BATCH_SIZE, class_weight='auto', shuffle=True, callbacks=callbacks_list)
 
 
     plot_training(history)
 
 elif args.mode == "predict":
 
-    # Create directories if needed
-    if not os.path.isdir("%s"%("Predictions")):
-            os.makedirs("%s"%("Predictions"))
-
     if args.image is None:
         ValueError("You must pass an image path when using prediction mode.")
 
+    # Create directories if needed
+    if not os.path.isdir("%s"%("Predictions")):
+        os.makedirs("%s"%("Predictions"))
+
+    # Read in your image
     image = cv2.imread(args.image,-1)
-    image = image.reshape(1, image.shape[0], image.shape[1], 3)
+    save_image = image
+    image = np.float32(cv2.resize(image, (HEIGHT, WIDTH)))
+    image = preprocessing_function(image.reshape(1, HEIGHT, WIDTH, 3))
 
     class_list_file = "./checkpoints/" + args.model + "_" + args.dataset + "_class_list.txt"
 
     class_list = load_class_list(class_list_file)
     
-    finetune_model = load_model("./checkpoints/" + args.model + "_" + args.dataset + "_model_weights.h5")
+    finetune_model = build_finetune_model(base_model, len(class_list))
+    finetune_model.load_weights("./checkpoints/" + args.model + "_model_weights.h5")
 
+    # Run the classifier and print results
     st = time.time()
 
     out = finetune_model.predict(image)
 
-    class_prediction = np.round(out)
-    class_name = class_list[int(class_prediction)]
+    confidence = out[0]
+    class_prediction = list(out[0]).index(max(out[0]))
+    class_name = class_list[class_prediction]
 
     run_time = time.time()-st
 
     print("Predicted class = ", class_name)
+    print("Confidence = ", confidence)
     print("Run time = ", run_time)
-
+    cv2.imwrite("Predictions/" + class_name[0] + ".png", save_image)
